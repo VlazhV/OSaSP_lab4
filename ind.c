474e7ba3e15dbe97ea1aa509f40c3dd9e4c743c5
vlazhv <danilabondarev1234@gmail.com>
2022-05-17 01:52:25 +0300

    one p - correct tree

--- ind.c
+++ ind.c
@@ -4,30 +4,25 @@
 
 int main()
 {
-
-	pid_t p0, p1, p2, p3, p4, p5, p6, p7;
 	
-	pid_t p;
-	
-	p0 = fork();
-	
-	switch (p0)
+	pid_t p = fork();
+	switch (p)
 	{
 		case -1: return -1;
 		case 0:
 			//proc 1;
 			printf("proc #%d, pid = %d, ppid = %d\n\n", 1, getpid(), getppid());
 			
-			p1 = fork();
-			switch (p1)
+			p = fork();
+			switch (p)
 			{
 				case -1: return -1;
 				case 0: 
 					//proc 2:
 					printf("proc #%d, pid = %d, ppid = %d\n\n", 2, getpid(), getppid());
 					
-					p4 = fork();
-					switch (p4)
+					p = fork();
+					switch (p)
 					{
 						case -1: return -1;
 						case 0:
@@ -37,24 +32,24 @@ int main()
 							exit(0);
 						default:
 							//proc 2
-							p5 = fork();
-							switch (p5)
+							p = fork();
+							switch (p)
 							{
 								case -1: return -1;
 								case 0: 
 									//proc 6
 									printf("proc #%d, pid = %d, ppid = %d\n\n", 6, getpid(), getppid());
 									
-									p6 = fork();
-									switch(p6)
+									p = fork();
+									switch(p)
 									{
 										case -1: return -1;
 										case 0:
 											//proc7
 											printf("proc #%d, pid = %d, ppid = %d\n\n", 7, getpid(), getppid());
 											
-											p7 = fork();
-											switch(p7)
+											p = fork();
+											switch(p)
 											{
 												case -1: return -1;
 												case 0:
@@ -86,8 +81,8 @@ int main()
 					exit(0);
 				default:
 				//proc 1;
-					p2 = fork();
-					switch (p2)
+					p = fork();
+					switch (p)
 					{
 						case -1: return -1;
 						case 0:
@@ -99,8 +94,8 @@ int main()
 						default:
 						//proc 1
 						
-							p3 = fork();
-							switch(p3)
+							p = fork();
+							switch(p)
 							{
 								case -1: return -1;
 								case 0:
